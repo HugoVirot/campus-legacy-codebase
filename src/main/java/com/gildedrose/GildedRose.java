050fd238564f960eceb232b9c719e8e416ffa1a6
Hugo Virot <hugo.virot@gmail.com>
2019-11-04 14:26:04 +0100

    Conjured ok, logs en cours

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,12 @@
 package com.gildedrose;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GildedRose {
+
+    Logger logger = LoggerFactory.getLogger(GildedRose.class);
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,26 +14,31 @@ public class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+
+        for (int i = 0; i < items.length; i++) {                  // on parcourt la liste des items
+            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//**********************cas général : si l'item n'est pas Aged Brie NI place concert*******************************
+                if (items[i].quality > 0) {                             //si sa qualité est positive
+                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {  //si ce n'est pas sulfuras
+                        if (items[i].name.matches("(.*)Conjured(.*)")) {           //si produit avec conjured dans nom
+                            items[i].quality = items[i].quality - 2;                    //sa qualité va baisser de 2
+                        } else
+                            items[i].quality = items[i].quality - 1;                    //sinon, sa qualité va baisser de 1
                     }
                 }
             } else {
+//*************************cas particuliers : place de concert, brie, sulfuras*************************************
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
+                        if (items[i].sellIn < 11) {         // concert avec 10 jours ou moins restants
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
+                        if (items[i].sellIn < 6) {          // concert avec 5 jours ou moins (rentre dans les deux cas : deux fois +1)
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
@@ -36,24 +47,24 @@ public class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {                         // si l'item n'est pas sulfuras
+                items[i].sellIn = items[i].sellIn - 1;                                           // jours -1
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (items[i].sellIn < 0) {                                      // si il n'y a plus de jours restants pour vendre le produit
+                if (!items[i].name.equals("Aged Brie")) {                                  // si pas brie
+                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { // si pas concert
+                        if (items[i].quality > 0) {                                            // qualité > 0
+                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {          // si pas sulfuras
+                                items[i].quality = items[i].quality - 1;                         // qualité - 1
                             }
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                    } else {                                                       // cas du concert
+                        items[i].quality = items[i].quality - items[i].quality;     // sa qualité passe à 0 car la date est passée
                     }
-                } else {
+                } else {                                                              // cas du brie
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                        items[i].quality = items[i].quality + 1;                      // sa qualité gagne +1 si inférieure à 50
                     }
                 }
             }
